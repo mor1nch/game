16282a0e2b0f3b32ce897316236bf584e66e9eff
mor1nch <110720280+mor1nch@users.noreply.github.com>
2022-08-06 13:12:28 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -40,6 +40,7 @@ pygame.init()
 screen_width = 1280
 screen_height = 720
 screen = pygame.display.set_mode([screen_width, screen_height])
+pygame.display.set_caption("Coding")
 
 all_sprites_list = pygame.sprite.Group()
 
